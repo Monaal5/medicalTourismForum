eab87c09983fc87997f3ef3a827b60bdf6c0c201
Monaal5 <125824648+Monaal5@users.noreply.github.com>
2025-11-20 16:28:04 +0530

    Add Google Tag Manager to layout component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -35,12 +35,39 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang="en">
-        <body
-          className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-        >
+        <head>
+          {/* Google Tag Manager */}
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+                (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+                })(window,document,'script','dataLayer','GTM-WJ9TCQ4J');
+              `,
+            }}
+          />
+          {/* End Google Tag Manager */}
+        </head>
+
+        <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+          
+          {/* Google Tag Manager (noscript) */}
+          <noscript>
+            <iframe
+              src="https://www.googletagmanager.com/ns.html?id=GTM-WJ9TCQ4J"
+              height="0"
+              width="0"
+              style={{ display: "none", visibility: "hidden" }}
+            ></iframe>
+          </noscript>
+          {/* End Google Tag Manager */}
+
           <QuoraHeader />
           <main className="flex-1">{children}</main>
           <Footer />
+          
           <Toaster
             position="top-right"
             toastOptions={{
@@ -65,6 +92,7 @@ export default function RootLayout({
               },
             }}
           />
+
         </body>
       </html>
     </ClerkProvider>
